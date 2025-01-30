cd50473661e9f7ed0dc5eb4681987087d0ada5e9
Nick Poholik <143164849+npoholik@users.noreply.github.com>
2025-01-30 17:30:30 -0600

    Fixed left and up buttons being switched, added directional input to pacman

--- Pacman.src/VGA.v
+++ Pacman.src/VGA.v
@@ -14,12 +14,11 @@
 // Revision:
 // Revision 0.01 - File Created
 //////////////////////////////////////////////////////////////////////////////////
-`define SCALE 4
 
-module VGA(
+module VGA #(parameter SCALE = 2) (
 	input clk,
 	input [11:0] switch,
-	input [3:0] cursor,			// Cursor controls pacman (0 = right, 1 = left, 2 = up, 3 = down)
+	input SW_up, SW_left, SW_right, SW_down,
 	output reg [3:0] vgaRed,
 	output reg [3:0] vgaBlue,
 	output reg [3:0] vgaGreen,
@@ -33,14 +32,19 @@ module VGA(
 	reg [3:0] iSprite, jSprite = 0;  // keep track of sprite color position
 	reg [9:0] pacX, pacY = 0;    	// keep track of sprite position
 	reg [1:0] frameSelect;  	// Select the current frame to draw
-	reg [1:0] direction; 		// Track the current direction of pacman in regards to 
+	reg [1:0] direction; 		// Track the current direction of pacman 
+    reg [3:0] cursor;
     
 	// Reserve enough space for a 640x480 resolution with enough space for front -> sync -> back
 	reg [9:0] xPix = 0;
 	reg [9:0] yPix = 0;
+    reg[3:0] frameUpdate;
     
 	Sprite sprite_inst(.x (iSprite),.y (jSprite), .select (frameSelect), .rgb (spriteRGB));
-
+    Buttons btn_up(SW_up, clk, cursor[0]);
+    Buttons btn_left(SW_left, clk, cursor[1]);
+    Buttons btn_right(SW_right, clk, cursor[2]);
+    Buttons btn_down(SW_down, clk, cursor[3]); 
     
 	// clock divider for 25 MHz
 	// for every 4 rising edges of master clock, clk_div will have a rising edge
@@ -53,6 +57,12 @@ module VGA(
         	counter <= counter + 1;
 	end
     
+    always @(posedge clk_div) begin
+        if(cursor[0]) direction = 0;
+        else if (cursor[1]) direction = 2;
+        else if (cursor[2]) direction = 1;
+        else if (cursor[3]) direction = 3;
+    end 
     
 	// color chooser
 	always @(posedge clk_div) begin
@@ -60,9 +70,23 @@ module VGA(
         	// Check for the bounds of pacman within the drawing of the screen (scaled up by 4)
         	if ((xPix >= pacX && xPix <= pacX + 16 * SCALE) && (yPix >= pacY && yPix <= pacY + 16 * SCALE)) begin
             	if (xPix % SCALE == 0) begin	// This mod 4 will determine which sprite color data to actually use
-					// This will draw pacman facing right 
-                	iSprite = (15 - ((yPix - pacY) / SCALE));
-                	jSprite = (15 - ((xPix - pacX) / SCALE));
+					// This will draw pacman facing left
+					if (direction == 2) begin 
+                	   iSprite = (15 - ((yPix - pacY) / SCALE));
+                	   jSprite = (15 - ((xPix - pacX) / SCALE));
+                	// this will draw pacman facing right 
+                	end else if (direction == 1) begin 
+                	   iSprite = ((yPix-pacY) / SCALE);
+                	   jSprite = ((xPix - pacX) / SCALE);
+                	// this will draw pacman facing up
+                	end else if (direction == 0) begin 
+                	   iSprite = (15 - ((xPix - pacX) / SCALE));
+                	   jSprite = (15 - ((yPix - pacY) / SCALE));
+                	// this will draw pacman facing down
+                	end else if (direction == 3) begin 
+                	   iSprite = ((xPix - pacX) / SCALE); 
+                	   jSprite = ((yPix - pacY) / SCALE); 
+                	end 
 				end
 
 				// Only draw non-transparent aspects of the sprite
@@ -115,18 +139,22 @@ module VGA(
         	Vsync <= 1;
 	end
     
-
-
-	reg [4:0] frameUpdate;
     
-	always @(yPix) begin
-    	if (yPix == 0) begin
-         	if (frameUpdate == 20) begin		// This should be 3 times a second
-            	frameSelect = frameSelect + 1;
-            	frameUpdate = 0;
-         	end
-         	frameUpdate = frameUpdate + 1;
-    	end
-	end
+    reg [4:0] directionUpdate;
+    
+    always @(posedge clk_div) begin 
+        if (yPix == 0 && xPix == 0) begin
+             if (frameUpdate == 5) begin
+                frameSelect = frameSelect + 1;
+                frameUpdate = 0;
+             end
+             //if (directionUpdate == 30) begin
+                //direction = direction + 1;
+             //   directionUpdate = 0;
+             //end
+             frameUpdate = frameUpdate + 1;
+             //directionUpdate = directionUpdate + 1;
+        end
+    end
     
 endmodule
