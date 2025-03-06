c17978b11cff84e798b46139941aad937397717f
Nick Poholik <143164849+npoholik@users.noreply.github.com>
2025-03-05 18:01:53 -0600

    Added additional needed map sprites

--- Pacman.src/Map_Sprite.v
+++ Pacman.src/Map_Sprite.v
@@ -23,6 +23,7 @@
 module Map_Sprite(
     input [4:0] GRID_SELECT,
     input [1:0] ROTATE_SELECT,
+    input [1:0] MIRROR_SELECT,
     input [2:0] X_INDEX,
     input [2:0] Y_INDEX,
     output reg [11:0] MAP_COLOR
@@ -32,19 +33,17 @@ module Map_Sprite(
 // We need to declare a sprite for each component of the maze grid 
 // Many of these components can be rotated in order to avoid redundancy 
 
-reg [11:0] mapBoundaryCorner [0:7][0:7];         // Use for corner map boundaries 
-mapNECorner = '{
+logic [11:0] mapBoundaryCorner [0:7][0:7] = '{   // Use for corner map boundaries 
     '{'h00F, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000, 'h000},
     '{'h00F, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000, 'h000},
-    '{'h00F, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000, 'h000},
-    '{'h00F, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000, 'h000},
-    '{'h00F, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000, 'h000},
-    '{'h00F, 'h000, 'h00F, 'h00F, 'h00F, 'h00F, 'h00F, 'h00F},
     '{'h00F, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000},
-    '{'h00F, 'h00F, 'h00F, 'h00F, 'h00F, 'h00F, 'h00F, 'h00F} };
+    '{'h000, 'h000, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000},
+    '{'h000, 'h00F, 'h000, 'h000, 'h00F, 'h00F, 'h00F, 'h00F},
+    '{'h000, 'h00F, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000},
+    '{'h000, 'h000, 'h00F, 'h00F, 'h000, 'h000, 'h000, 'h000},
+    '{'h000, 'h000, 'h000, 'h000, 'h00F, 'h00F, 'h00F, 'h00F} };
 
-reg[11:0] mapBoundaryWall [0:7][0:7];  // Utilized for vertical map wall boundaries
-mapWStraightVertical = '{
+logic [11:0] mapBoundaryWall[0:7][0:7] = '{     // Utilized for vertical map wall boundaries
     '{'h00F, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000, 'h000},
     '{'h00F, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000, 'h000},
     '{'h00F, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000, 'h000},
@@ -54,8 +53,17 @@ mapWStraightVertical = '{
     '{'h00F, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000, 'h000},
     '{'h00F, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000, 'h000} };
 
-reg[11:0] mapInteriorWall [0:7][0:7]; // Utilized for defining interior walls of the map 
-mapInteriorWall = '{
+logic [11:0] mapBoundaryHalfT[0:7][0:7] = '{     // Utilize for instances where the boundary border extends into the interior of the map 
+    '{'h00F, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000, 'h000},
+    '{'h00F, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000, 'h000},
+    '{'h00F, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000, 'h000},
+    '{'h00F, 'h000, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000},
+    '{'h00F, 'h000, 'h000, 'h000, 'h00F, 'h000, 'h000, 'h000},
+    '{'h00F, 'h000, 'h000, 'h000, 'h000, 'h00F, 'h00F, 'h00F},
+    '{'h00F, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000},
+    '{'h00F, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000} }; 
+
+logic [11:0] mapInteriorWall [0:7][0:7] = '{       // Utilized for defining interior walls of the map 
     '{'h000, 'h000, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000},
     '{'h000, 'h000, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000},
     '{'h000, 'h000, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000},
@@ -65,19 +73,27 @@ mapInteriorWall = '{
     '{'h000, 'h000, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000},
     '{'h000, 'h000, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000} };
 
-reg[11:0] mapInteriorCorner [0:7][0:7];  // Utilized for defining interior corners of the map 
-mapInteriorCorner = '{
-    '{'h000, 'h000, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000},
-    '{'h000, 'h000, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000},
+logic [11:0] mapShallowCorner [0:7][0:7] = '{     // Utilized for defining interior corners of the map 
+    '{'h000, 'h000, 'h000, 'h000, 'h00F, 'h000, 'h000, 'h000},
     '{'h000, 'h000, 'h000, 'h000, 'h00F, 'h000, 'h000, 'h000},
     '{'h000, 'h000, 'h000, 'h000, 'h000, 'h00F, 'h000, 'h000},
     '{'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h00F, 'h00F},
     '{'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000},
     '{'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000},
+    '{'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000},
     '{'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000} };
 
-reg[11:0] mapBlank [0:7][0:7];  // Represents free space for pacman to move
-mapBlank = '{
+logic [11:0] mapDeepCorner [0:7][0:7] = '{     // Utilized for cases in the map drawing where normal corners would be off by 1 pixel 
+    '{'h000, 'h000, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000},
+    '{'h000, 'h000, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000},
+    '{'h000, 'h000, 'h000, 'h00F, 'h000, 'h000, 'h000, 'h000},
+    '{'h000, 'h000, 'h000, 'h000, 'h00F, 'h000, 'h000, 'h000},
+    '{'h000, 'h000, 'h000, 'h000, 'h000, 'h00F, 'h00F, 'h00F},
+    '{'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000},
+    '{'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000},
+    '{'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000} };
+
+logic [11:0] mapBlank [0:7][0:7]= '{       // Represents free space for pacman to move
     '{'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000},
     '{'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000},
     '{'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000},
@@ -86,5 +102,16 @@ mapBlank = '{
     '{'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000},
     '{'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000},
     '{'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000} };
+
+logic [11:0] mapGhostGate [0:7][0:7] = '{       // White gate where the ghosts exit out of in map 
+    '{'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000},
+    '{'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000},
+    '{'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000},
+    '{'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000},
+    '{'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000},
+    '{'hFFF, 'hFFF, 'hFFF, 'hFFF, 'hFFF, 'hFFF, 'hFFF, 'hFFF},
+    '{'hFFF, 'hFFF, 'hFFF, 'hFFF, 'hFFF, 'hFFF, 'hFFF, 'hFFF},
+    '{'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000, 'h000} };
+  
 
 endmodule
